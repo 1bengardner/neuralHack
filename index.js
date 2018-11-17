07cda525268eb7d14ede4eadef0148ecc300c54a
Daniel <chosww@gmail.com>
2018-11-17 10:50:08 -0500

    download youtube song to a directory

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const rndsong = require("rnd-song");
 const genreCode = require("rnd-song/genres.json");
 const request = require("request");
 const api_keys = require("./config.json");
+const fs = require("fs");
+const ytdl = require("ytdl-core");
+const play = require("audio-play");
+const load = require("audio-loader");
 
 const numGenres = Object.keys(genreCode).length;
 const randomGenreIndex = Math.floor(Math.random() * Math.floor(numGenres - 1));
@@ -14,23 +18,30 @@ const options = {
   language: "en"
 };
 
-rndsong(options, function(err, res) {
-  if (!err) {
-    request.get(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${
-        res.track.track_name
-      }&type=music&key=${api_keys.googleApi}`,
-      (error, response, body) => {
-        if (!error) {
-          console.log(
-            `https://www.youtube.com/watch?v=${
-              JSON.parse(body).items[0].id.videoId
-            }`
-          );
+async function getRandomSong() {
+  rndsong(options, function(err, res) {
+    if (!err) {
+      request.get(
+        `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${
+          res.track.track_name
+        }&type=music&key=${api_keys.googleApi}`,
+        (error, response, body) => {
+          if (!error) {
+            ytdl(
+              `https://www.youtube.com/watch?v=${
+                JSON.parse(body).items[0].id.videoId
+              }`
+            ).pipe(fs.createWriteStream("./music/test.mp3"));
+          }
         }
-      }
-    );
-  } else {
-    console.log(err);
-  }
-});
+      );
+    } else {
+    }
+  });
+}
+
+async function playSong() {
+  const randSong = await getRandomSong();
+}
+
+playSong();
